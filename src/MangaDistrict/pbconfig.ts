1603b7ad251b842f741f5fd02b754e3a0272a20e
Eduard Smet <contact@celarye.dev>
2026-01-13 23:13:30 +0100

    feat(MangaDistrict, YaoiScan): Add extensions

---
+++ src/MangaDistrict/pbconfig.ts
@@ -0,0 +1,25 @@
+import { ContentRating, type ExtensionInfo, SourceIntents } from "@paperback/types";
+import { getVersion } from "../generic/utils";
+
+export default {
+  name: "MangaDistrict",
+  description: "Extension that pulls content from mangadistrict.com.",
+  version: getVersion({ increasePrerelease: -3 }),
+  icon: "icon.png",
+  language: "en",
+  contentRating: ContentRating.ADULT,
+  badges: [],
+  capabilities:
+    SourceIntents.CHAPTER_PROVIDING |
+    SourceIntents.DISCOVER_SECIONS_PROVIDING |
+    SourceIntents.SETTINGS_FORM_PROVIDING |
+    SourceIntents.SEARCH_RESULTS_PROVIDING |
+    SourceIntents.CLOUDFLARE_BYPASS_PROVIDING,
+  developers: [
+    {
+      name: "Inkdex",
+      website: "https://inkdex.github.io",
+      github: "https://github.com/inkdex",
+    },
+  ],
+} satisfies ExtensionInfo;
