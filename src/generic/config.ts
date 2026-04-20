598d5d49803cab0e167d779949a48b4e33725f62
Nicholas Lim <6513413+niclimcy@users.noreply.github.com>
2026-04-21 00:46:43 +0800

    chore: Bump generic pre-release version

--- src/generic/config.ts
+++ src/generic/config.ts
@@ -3,7 +3,7 @@
 
 import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/types";
 
-const BASE_VERSION = "1.0.0-alpha.8";
+const BASE_VERSION = "1.0.0-alpha.9";
 
 export const basePbConfig = {
   name: "",
